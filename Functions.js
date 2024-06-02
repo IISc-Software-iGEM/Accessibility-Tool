ec22c15be9b2d9f7d7ca8b9f7e627ba426d2f20c
Yash Kamble <142909905+YasKam24@users.noreply.github.com>
2024-06-02 09:50:09 +0530

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,7 +1,6 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
-
   
 	const accessibilityMenuURL = './Toolbar.html'; 	
 	const stylesURL = './styles.scss'; 
@@ -12,46 +11,42 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.head.appendChild(link);
 
 	fetch(stylesURL)
-		.then(response => response.text()) // Parse response as CSS text
+		.then(response => response.text())
 		.then(cssText => {
-			// Create style element and set content
 			var styleTag = document.createElement('style');
 			styleTag.textContent = cssText;
 			document.head.appendChild(styleTag);
   
-			// Fetch accessibility tool HTML content
 			return fetch(accessibilityMenuURL);
 		})
-		.then(response => response.text()) // Parse response as HTML text
+		.then(response => response.text())
 		.then(htmlContent => {
-			// Create accessibility tool element and set content
 			var createToolBox = document.createElement('div');
 			createToolBox.id = 'init-access-tool';
 			createToolBox.innerHTML = htmlContent;
   
 			document.body.insertBefore(createToolBox, document.body.firstChild);
 			const accessToolButton = document.querySelector(".accesstool");
-		        const accessToolBar = document.querySelector(".access-tool-bar");
-		        const closeButton = document.querySelector(".close-button");
-		  
-		        function toggleAccessibilityToolBar() {
-		          if (accessToolBar.style.visibility === "hidden") {
-		            accessToolBar.style.visibility = "visible";
-		            accessToolBar.style.opacity = 1; // Add opacity for smooth transition
-		          } else {
-		            accessToolBar.style.visibility = "hidden";
-		            accessToolBar.style.opacity = 0; // Add opacity for smooth transition
-		          }
-		        }
-		  
-		        accessToolButton.addEventListener("click", toggleAccessibilityToolBar);
-		        closeButton.addEventListener("click", toggleAccessibilityToolBar);
+			const accessToolBar = document.querySelector(".access-tool-bar");
+			const closeButton = document.querySelector(".close-button");
+		
+			function toggleAccessibilityToolBar() {
+			if (accessToolBar.style.visibility === "hidden") {
+				accessToolBar.style.visibility = "visible";
+				accessToolBar.style.opacity = 1; 
+			} else {
+				accessToolBar.style.visibility = "hidden";
+				accessToolBar.style.opacity = 0;
+			}
+			}
+		
+			accessToolButton.addEventListener("click", toggleAccessibilityToolBar);
+			closeButton.addEventListener("click", toggleAccessibilityToolBar);
 		})
 		.catch(error => {
 			console.error('Error fetching resources:', error);
-			// Handle errors gracefully (e.g., display an error message)
+			
 		});
 
-		
+
 });
-  
